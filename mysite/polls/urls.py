c452112dee795c8563b822b78e663e22f44401e8
Sanjiv <kv.vajra@gmail.com>
2018-03-28 11:46:29 +0545

    secound commit

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,12 +3,12 @@ from . import views
 
 urlpatterns = [
 
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
     # 127.0.0.1/polls/
-    url(r'^(?P<question_id>[0-9]+1/$', views.detail, name="detail"),
-    #127.0.0.1/polls/1
-    url(r'^(?P<question_id>[0-9]+1/result$', views.result, name="results"),
-    #127.0.0.1/polls/1/results
-    url(r'^(?P<question_id>[0-9]+1/vote$', views.vote, name="vote"),
-    #127.0.0.1/polls/1/votes
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name="detail"),
+    # 127.0.0.1/polls/1
+    url(r'^(?P<question_id>[0-9]+)/results$', views.results, name="results"),
+    # 127.0.0.1/polls/1/results
+    url(r'^(?P<question_id>[0-9]+)/vote$', views.vote, name="vote"),
+    # 127.0.0.1/polls/1/vote
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Awesome, Index page of application')
+    latest_questions = Question.objects.order_by('-pub_date')[:5]
+    output = ", ".join(q.question_text for q in latest_questions)
+    return HttpResponse(output)
 
 def detail(request, question_id):
-    return HttpResponse("deteil version of question %s" % question_id)
+    return HttpResponse("detail version of question %s" % question_id)
 
-def result(request, question_id):
+def results(request, question_id):
     return HttpResponse("detail vew of answer %s" % question_id)
 
 def vote(request, question_id):
