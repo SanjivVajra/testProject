a6551946547ac9328bb198f6d8aa58c40371ae2b
Sanjeev Bajra Shakya <kv.vajra@gmail.com>
2018-03-28 16:08:15 +0545

    secound commit

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+# from django.template import loader, RequestContext
 from .models import Question
 
 
@@ -7,8 +8,12 @@ from .models import Question
 
 def index(request):
     latest_questions = Question.objects.order_by('-pub_date')[:5]
-    output = ", ".join(q.question_text for q in latest_questions)
-    return HttpResponse(output)
+    # output = ", ".join(q.question_text for q in latest_questions)
+    #template = loader.get_template('polls/index.html')
+    context = {'latest_questions': latest_questions}
+    return render(request, 'polls/index.html', context)
+    # return HttpResponse(template.render(context))
+    # return HttpResponse(output)
 
 def detail(request, question_id):
     return HttpResponse("detail version of question %s" % question_id)
